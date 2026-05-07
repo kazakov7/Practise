2a661fcbdb79df3b68ac9eb5c76687ea4803ae9b
Kevin <kazakov28082003@gmail.com>
2026-05-07 18:54:55 +0900

    python system variables & pecial methods via dunder

--- run.py
+++ run.py
@@ -1,1 +1,18 @@
+<<<<<<< HEAD
 print("Hello world")
+=======
+# Dunder __Builtins__, __init__
+message = "Python: everthying is Object "
+print(message)
+
+result = type(message)
+print("result:", result)
+
+'''In Python, there a builtin tools:
+(1)Types: int float string list dict
+(2)Functions: print() len() input() type() str() int()
+(3)Contstants: True False None
+'''
+
+print(dir(__builtins__))
+>>>>>>> a761b68 (python system variables & pecial methods via dunder)
