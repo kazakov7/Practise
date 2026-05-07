cd29ab42a24aaca9bf37da0d15553280e6599d83
Kevin <kazakov28082003@gmail.com>
2026-05-07 19:00:13 +0900

    feat: learn numbers i  python

--- run.py
+++ run.py
@@ -12,3 +12,16 @@
 # '''
 
 # print(dir(__builtins__))
+
+
+print("≈≈≈≈≈≈ Number≈≈≈≈≈≈≈")
+# in Java: variable is a name storage location!
+# in Python: variable is named reference!
+
+count = 100
+count_type = type(count)
+print(f'the count : {count} and type: {count_type}')
+
+result1 = count.bit_count()
+result2 = count.numerator
+print(result1, result2)
